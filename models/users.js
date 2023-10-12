5272ffad3f72dfb8a25de2d256aecd64c3e3d7b6
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-10-12 16:14:10 +0700

    Update users.js models for db

--- models/users.js
+++ models/users.js
@@ -2,22 +2,37 @@ const mongoose = require('mongoose')
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const Users = mongoose.model('users', {
-    email: {
-        type: String, 
-        required: true
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+  },
+  sexuality: {
+    type: String,
+  },
+  counselors: [
+    {
+      name: String,
+      interests: String,
+      religion: String,
+      book: String,
+      goals: String,
     },
-    name: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: Number,
-        required: true
-    }
+  ],
 })
 
 module.exports = Users
