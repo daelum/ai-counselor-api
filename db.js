cf393267679b296dbb5ba4b03ac7dca8c0a32d20
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-10-12 16:12:20 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 module.exports = {
-    DB_URL: 'mongodb+srv://daelum:fVzGwRXQrzHeF1Yu@cluster0.tnfj5io.mongodb.net/aicounselor',
-    SESSION_SECRET: 'YOUR_SECRET'
+  DB_URL:
+    'mongodb+srv://daelum:fVzGwRXQrzHeF1Yu@cluster0.tnfj5io.mongodb.net/aicounselor',
+  SESSION_SECRET: 'YOUR_SECRET',
 }
