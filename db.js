ed34bcb6e52e13622fe52a3274fa93b084e4e4da
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-11-08 15:17:13 +1100

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
+
 module.exports = {
-  DB_URL:
-    'mongodb+srv://daelum:fVzGwRXQrzHeF1Yu@cluster0.tnfj5io.mongodb.net/aicounselor',
+  DB_URL: `mongodb+srv://daelum:${process.env.DB_KEY}@cluster0.tnfj5io.mongodb.net/aicounselor`,
   SESSION_SECRET: 'YOUR_SECRET',
 }
